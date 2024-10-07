197efff7405f507e7246f6f0ac98bcc4a0a03a22
ficky febrian <iky.febrian@gmail.com>
2024-10-07 20:10:58 +0700

    on progress

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Portfolio = () => {
   return (
     <div className="h-screen flex justify-center text-center items-center text-6xl font-playfair">
-      Its Portfolio!
+      On progress..
     </div>
   );
 };
